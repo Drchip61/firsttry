218b8e8bdd6b651e4f64aaa9e3b20ac24494b5df
Drchip61 <75516995+Drchip61@users.noreply.github.com>
2021-04-21 01:03:41 +0800

    Add files via upload

---
+++ 长路漫漫python试炼1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 21:36:51 2021
+
+@author: 严天宇
+"""
+
+import torch
+import torch.nn as nn
+import math
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model=10, dropout=0.1, max_len=50):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        '''
+        x: [seq_len, batch_size, d_model]
+        '''
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
